70e10d841c808c1e862ef53dd5eef52b8a591900
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-03-19 12:51:25 -0500

    first commit

---
+++ src/pickNumbers/Main.java
@@ -0,0 +1,15 @@
+package pickNumbers;
+
+import java.util.Scanner;
+
+public class Main {
+	public static int[] getArray() {
+		return null;
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+}
+}
---
+++ src/pickNumbers/Userinput.java
@@ -0,0 +1,21 @@
+package pickNumbers;
+
+import java.util.*;
+
+public class Userinput {
+	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter first number:");
+		int a = sc.nextInt();
+		System.out.print("Enter seccond number:");
+		int b = sc.nextInt();
+		System.out.print("Enter third number:");
+		int
+	}
+		
+	}
+	
+
+
